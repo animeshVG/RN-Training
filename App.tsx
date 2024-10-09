71c18ee38f154f6aabe35ecc295da11d0b24bc15
animeshVG <animesh.raikwar@vinzglobal.com>
2024-10-09 14:16:13 +0530

    ScrollView

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [change, setChange] = useState("");
+  const [goals, setGoals] = useState([]);
+  const goalInput = (event) => {
+    setChange(event);
+  }
+
+  const adGoal = () => {
+    setGoals(currentGoals => [...currentGoals, change]);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={goalInput} style={styles.textInput} placeholder='Your Course Goal' />
+        <Button onPress={adGoal} title='Add Goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+      <ScrollView alwaysBounceVertical={false}>
+        {
+          goals.map((goal) =>
+          ((<View style={styles.goalItem} key={goal}><Text style={{ color: "white" }}>{goal}</Text></View>)
+          ))
+        }
+      </ScrollView>
+      </View>
     </View>
   )
 }
@@ -12,12 +36,35 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({
-container:{
-  height:"100%",
-  width:"100%",
-  display:"flex",
-  backgroundColor:"yellow",
-  justifyContent:"center",
-  alignItems:"center"
-}
+  container: {
+    flex: 1,
+    padding: 50,
+    paddingHorizontal: 16
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "grey"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "blue",
+    width: "70%",
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 4
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+    color: "white"
+  }
 })
