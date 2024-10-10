5d19fd931d86f407279ab9d7f5df25f9856524d2
animeshVG <animesh.raikwar@vinzglobal.com>
2024-10-10 11:36:14 +0530

    Starting the day 10-10-24

--- App.tsx
+++ App.tsx
@@ -1,75 +1,20 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList} from 'react-native'
-import React from 'react'
-import { useState } from 'react'
-import GoalItem from './components/GoalItem'
-import GoalInput from './components/GoalInput'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  const [modalIsVisible, setmodalIsVisible] = useState(false);
-  const [goals, setGoals] = useState([]);
-
-
-  const startAddGoalHandler = () => {
-    setmodalIsVisible(true);
-  }
-
-  function endAddGoalHandler() {
-    setmodalIsVisible(false)
-  }
-
-  const adGoal = (change) => {
-    setGoals(currentGoals => [...currentGoals, { text: change, id: Math.random().toString() }]);
-    endAddGoalHandler();
-  }
-
-
-  const deleteGoalHandler = (id) => {
-    setGoals(currentGoals => {
-      return currentGoals.filter((goals) => goals.id !== id);
-    });
-
-  }
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler} />
-      <GoalInput visible={modalIsVisible} OnAddGoal={adGoal} onCancle={endAddGoalHandler} />
-      {/* <View style={styles.inputContainer}>
-        <TextInput onChangeText={goalInput} style={styles.textInput} placeholder='Your Course Goal' />
-        <Button onPress={adGoal} title='Add Goal' />
-      </View> */}
-      <View style={styles.goalsContainer}>
-        <FlatList data={goals} renderItem={itemData => {
-          return (
-            <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler} id={itemData.item.id} />
-          )
-        }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-        />
-        {/* <ScrollView alwaysBounceVertical={false}>
-        {
-          goals.map((goal) =>
-          ((<View style={styles.goalItem} key={goal}><Text style={{ color: "white" }}>{goal}</Text></View>)
-          ))
-        }
-      </ScrollView> */}
-      </View>
+      <Text>Starting the day</Text>
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
-
-export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
-    paddingHorizontal: 16,
-    backgroundColor:"#311b6b"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  goalsContainer: {
-    flex: 4
-  }
-})
+});
