f8aaedf68c6fb250e6595d69cd709a99aac3cc06
animeshVG <animesh.raikwar@vinzglobal.com>
2024-10-10 14:23:19 +0530

    Linear Gradient Added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./Screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#ddb52f","#4e0329"]} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
- 
+  rootScreen:{
+    flex:1,
+  }
 });
